5ad5f92fde8eaf726a1e5adafc88f247e7e4420c
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-03-24 18:13:50 +0100

    morgan added

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express =require('express');
-const app =express();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 const PORT = 3000;
-const productsRouter =require('./routes/products.js');
-const categoriesRouter =require('./routes/categories.js');
-const usersRouter =require('./routes/users.js');
-const ordersRouter =require('./routes/orders.js');
+const productsRouter = require('./routes/products.js');
+const categoriesRouter = require('./routes/categories.js');
+const usersRouter = require('./routes/users.js');
+const ordersRouter = require('./routes/orders.js');
 
 app.use(express.json());
+app.use(morgan('dev'));
 
-app.use('/categories',categoriesRouter);
-app.use('/products',productsRouter);
-app.use('/users',usersRouter);
-app.use('/orders',ordersRouter);
+app.use('/categories', categoriesRouter);
+app.use('/products', productsRouter);
+app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
 
-app.listen(PORT,()=>console.log('server running on port '+PORT));
+app.listen(PORT, () => console.log('server running on port ' + PORT));
